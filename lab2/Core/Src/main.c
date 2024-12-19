d9d81e3da291520b5852826aa6d52f0f0fb0cfe0
Alf1rAP <alfirap1@yandex.ru>
2024-12-20 01:09:35 +0300

    Task1 Completed

--- lab2/Core/Src/main.c
+++ lab2/Core/Src/main.c
@@ -1,20 +1,25 @@
-#include "main.h"
 #include "stm32f4xx.h"
 
-int main (void)
+int main(void)
 {
-	uint32_t i;
+    uint32_t i;
 
-	RCC->AHB1ENR|=RCC_AHB1ENR_GPIODEN;
+    // Включаем тактирование порта GPIOD для светодиодов
+    RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN;
 
-	GPIOD->MODER = 0x55000000;
-	GPIOD->OTYPER = 0;
-	GPIOD->OSPEEDR = 0;
-	while (1)
-	{
-		GPIOD->ODR = 0xF000;
-		for (i=0;i<500000;i++){}
-		GPIOD->ODR = 0x0000;
-		for (i=0;i<500000;i++){}
-	}
+    // Настроим пины GPIOD на выход (для светодиодов)
+    GPIOD->MODER |= 0x55000000;   // Пины 12-15 на выход
+    GPIOD->OTYPER &= ~0x0000;     // Выходы с подтягивающими резисторами
+    GPIOD->OSPEEDR &= ~0x00000000; // Нормальная скорость
+
+    while (1)
+    {
+        // Включаем светодиод по очереди
+        for (i = 0; i < 4; i++) {
+            GPIOD->ODR = (1 << (12 + i));  // Включаем светодиод
+            for (volatile uint32_t delay = 0; delay < 500000; delay++) {}  // Задержка
+            GPIOD->ODR &= ~(1 << (12 + i)); // Выключаем светодиод
+            for (volatile uint32_t delay = 0; delay < 500000; delay++) {}  // Задержка
+        }
+    }
 }
